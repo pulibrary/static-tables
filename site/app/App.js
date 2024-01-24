13effb4457f02ad89cfb9aebc25b0982eec5b17b
Jane Sandberg <js7389@princeton.edu>
2024-01-24 12:28:16 -0800

    Starting work toward a separate utilty class for sorting
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>

--- site/app/App.js
+++ site/app/App.js
@@ -1,6 +1,7 @@
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 import PaginatedTable from './components/PaginatedTable.js';
+import { sortByDate } from './utilities/SortingUtilities.js';
 
 export default {
   name: 'App',
@@ -16,7 +17,7 @@ export default {
         <img :src="'./assets/images/marquand-banner_0.jpg'" class="img-fluid" width="1200" height="265" alt="geometric pattern" />
         <h2 class="bg-black text-white">Marquand Library of Art and Archaeology</h2>
       </div>
-      <PaginatedTable></PaginatedTable>
+      <PaginatedTable sorter="sortByDate"></PaginatedTable>
     </div>
     <Footer></Footer>`
 }
