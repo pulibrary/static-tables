6e10556c4bd1b1806e8326590cdc6a051b65c024
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-05-14 14:40:34 -0400

    Fixed typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 server "static-tables-prod1", user: "deploy", roles: %w{app web}
-server "statid-tables-prod2", user: "deploy", roles: %w{app web}
+server "static-tables-prod2", user: "deploy", roles: %w{app web}
