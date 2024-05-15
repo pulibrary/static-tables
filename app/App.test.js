91953727241556c3536b6f6a8fc02eebb6e1ff2b
Max Kadel <mkadel@princeton.edu>
2024-05-15 11:02:32 -0400

    WIP - Start routing, test failing probably right reason
    Co-authored-by: Christina Chortaria <actspatial@gmail.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- app/App.test.js
+++ app/App.test.js
@@ -1,22 +1,30 @@
-import { describe, test, expect, vi } from 'vitest';
+import { describe, test, expect, vi, beforeEach } from 'vitest';
 import { mount } from '@vue/test-utils';
 import App from './App.js';
 
+let wrapper;
 describe('App', () => {
+  beforeEach(() => {
+    wrapper = mount(App, {});
+  });
   test('page title', () => {
-    const wrapper = mount(App, {
-      props: {}
-    });
     expect(wrapper.find('.page-title').text()).toBe(
       'Marquand Library of Art and Archaeology'
     );
   }),
     test('header title', () => {
-      const wrapper = mount(App, {
-        props: {}
-      });
       expect(wrapper.find('.header-title').text()).toBe(
         'Marquand Sales Catalogs'
       );
     });
+  describe('Routing', () => {
+    test('changing page title by url', () => {
+      Object.defineProperty(window, 'location', {
+        hash: '/faculty-and-professional-staff-index'
+      });
+      expect(wrapper.find('.page-title').text()).toBe(
+        'Faculty and Professional Staff Index, 1764-2006'
+      );
+    });
+  });
 });
--- app/configs/DataMetadata.js
+++ app/configs/DataMetadata.js
@@ -7,5 +7,12 @@ export default {
                   Many current sales catalogs from Christie's, Sotheby's, Bonham's, Phillips, Swann, William Doyle and others have been boxed by city and date and are mostly off-site out at ReCAP (browse below). \
                   These require 1-2 business days' notice at least and up to a week if many catalogs are needed. Marquand has many more catalogs from c. 1820s-1990s for more than 250 auction houses, both cataloged and un-cataloged. \
                   Book and coin sales, unless mixed content, are handled by Special Collections in Firestone Library.</p>`
+  },
+  faculty_and_staff: {
+    header_title: 'Faculty and Professional Staff',
+    page_title: 'Faculty and Professional Staff Index, 1764-2006',
+    table_title: 'Browse Faculty and Professional Staff',
+    description:
+      'Index for Faculty & Professional Research, Technical & Library Personnel files, 1764-2004. Contains the name, death date, departure date, and department for Princeton University personnel. (Files for some trustees, administrators, and others may also be found).<a href="https://findingaids.princeton.edu/collections/AC107#accessuse">Explanation of&nbsp;Access to personnel files.</a>'
   }
 };
