4b0f9c7822098e805b38b1cd0a6d83741d882b8c
Max Kadel <mkadel@princeton.edu>
2024-05-17 10:04:14 -0400

    Try different build paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,8 @@
+# Default branch is :main
 set :branch, ENV['BRANCH'] || 'main'
 
 set :application, "static_tables"
 set :repo_url, "https://github.com/pulibrary/static-tables.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/opt/static_tables"
