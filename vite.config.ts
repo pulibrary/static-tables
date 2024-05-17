04baa1ab07f07905e57c3cfa863beafe5229bffe
Christina Chortaria <actspatial@gmail.com>
2024-05-17 14:15:44 -0400

    Update vite.config to use loadEnv and get variables from .env
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    cors: false
-  },
-  base: process.env.VITE_STATIC_TABLES_BASE_PATH
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vue()],
+    server: {
+      cors: false
+    },
+    base: env.VITE_STATIC_TABLES_BASE_PATH
+  };
 });
