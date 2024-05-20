e759ba154d3996667d977f5f76547c09d4f8c361
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-05-20 16:34:12 -0400

    Refactored routes

--- app/App.js
+++ app/App.js
@@ -1,26 +1,7 @@
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 import PaginatedTable from './components/PaginatedTable.js';
-import Marquand from './configs/Marquand.js';
-import FacultyAndStaff from './configs/FacultyAndStaff.js';
-import GraduateAlumniIndex from './configs/GraduateAlumniIndex.js';
-import HonoraryDegree from './configs/HonoraryDegree.js';
-import PrincetonAlumniMemorial from './configs/PrincetonAlumniMemorial.js';
-import TrusteeIndex from './configs/TrusteeIndex.js';
-import UndergradAlumniIndexPart1 from './configs/UndergradAlumniIndexPart1.js';
-import WorldWarII from './configs/WorldWarII.js';
-
-// prettier-ignore
-const routes = {
-  'marquand-catalogs': 'marquand',
-  'faculty-and-professional-staff-index': 'faculty_and_staff',
-  'princeton-university-graduate-alumni-index': 'graduate_alumni_index',
-  'honorary-degree-index': 'honorary_degree',
-  'princeton-alumni-weekly-memorial-index': 'princeton_alumni_memorial',
-  'trustee-index-1746-2001': 'trustee_index',
-  'undergraduate-alumni-index-part-1': 'undergrad_alumni_index_part_1',
-  'world-war-ii-memorial-book': 'world_war_ii_memorial'
-};
+import Routes from './configs/Routes.js';
 
 export default {
   name: 'App',
@@ -31,42 +12,15 @@ export default {
   },
   computed: {
     metadataConfig() {
-      switch (this.desiredRoute) {
-        case 'marquand':
-          return Marquand;
-        case 'faculty_and_staff':
-          return FacultyAndStaff;
-        case 'graduate_alumni_index':
-          return GraduateAlumniIndex;
-        case 'honorary_degree':
-          return HonoraryDegree;
-        case 'princeton_alumni_memorial':
-          return PrincetonAlumniMemorial;
-        case 'trustee_index':
-          return TrusteeIndex;
-        case 'undergrad_alumni_index_part_1':
-          return UndergradAlumniIndexPart1;
-        case 'world_war_ii_memorial':
-          return WorldWarII;
-        default:
-          return Marquand;
-      }
-    },
-    desiredRoute() {
       const routePath = window.location.pathname
         .split('/')
         .filter(n => n)
         .pop();
-      return routes[routePath] || routes['marquand'];
+      return Routes[routePath] || Routes['marquand-catalogs'];
     },
     sorterMethod() {
-      if (this.metadataConfig.sorter_method) {
-        return this.metadataConfig.sorter_method;
-      } else {
-        return () => {
-          return 1;
-        };
-      }
+      const defaultSorter = () => 1;
+      return this.metadataConfig.sorterMethod || defaultSorter;
     },
     bannerUrl() {
       return this.metadataConfig.banner_url || null;
---
+++ app/configs/Routes.js
@@ -0,0 +1,19 @@
+import Marquand from './Marquand.js';
+import FacultyAndStaff from './FacultyAndStaff.js';
+import GraduateAlumniIndex from './GraduateAlumniIndex.js';
+import HonoraryDegree from './HonoraryDegree.js';
+import PrincetonAlumniMemorial from './PrincetonAlumniMemorial.js';
+import TrusteeIndex from './TrusteeIndex.js';
+import UndergradAlumniIndexPart1 from './UndergradAlumniIndexPart1.js';
+import WorldWarII from './WorldWarII.js';
+
+export default {
+  'marquand-catalogs': Marquand,
+  'faculty-and-professional-staff-index': FacultyAndStaff,
+  'princeton-university-graduate-alumni-index': GraduateAlumniIndex,
+  'honorary-degree-index': HonoraryDegree,
+  'princeton-alumni-weekly-memorial-index': PrincetonAlumniMemorial,
+  'trustee-index-1746-2001': TrusteeIndex,
+  'undergraduate-alumni-index-part-1': UndergradAlumniIndexPart1,
+  'world-war-ii-memorial-book': WorldWarII
+};
