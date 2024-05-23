a4d81428e054472fd896e4cba601cd2fd438015c
Max Kadel <mkadel@princeton.edu>
2024-05-23 13:42:14 -0400

    Avoid misleading autofill in browsers
    - When you use "name" for a form field, browsers tend to try to fill it with the user's contact information, which is misleading for this form.
    - Use input type="search" rather than text, per [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/searchbox_role#description)
    
    Closes #113

--- app/components/TextFilter.js
+++ app/components/TextFilter.js
@@ -2,7 +2,7 @@ export default {
   name: 'TextFilter',
   template: `
       <label :for="config.id" class="form-label">{{ config.name }}</label>
-      <input class="form-control" :id="config.id" :aria-label="config.aria_label" type="text" @input="handleInput">
+      <input class="form-control" :id="config.id" :aria-label="config.aria_label" type="search" @input="handleInput">
       `,
   props: ['config'],
   emits: ['filterChange'],
