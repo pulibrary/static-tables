6b367ec30932ac4e682cc5d2e5a8ae22c173abb0
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-05-24 10:51:19 -0400

    Added honeybadger config

--- app/main.js
+++ app/main.js
@@ -2,8 +2,20 @@ import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import HoneybadgerVue from '@honeybadger-io/vue';
+import Honeybadger from '@honeybadger-io/js';
 import 'lux-design-system/dist/style.css';
 
 const app = createApp(App);
 
+const config = {
+  apiKey: env.VITE_HONEYBADGER_API_KEY,
+  environment: env.VITE_HONEYBADGER_ENV
+};
+
+app.use(HoneybadgerVue, config);
+
 app.mount('#app');
+
+// TODO: Remove after testing
+Honeybadger.notify('Testing Honeybadger!');
