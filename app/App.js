1124a4d5d13c35d5ef3758b86f3d58e3091e36dd
Christina Chortaria <actspatial@gmail.com>
2024-07-12 16:19:55 -0400

    [#179] Update page-title background-color to --color-grayscale-dark

--- app/App.js
+++ app/App.js
@@ -32,7 +32,7 @@ export default {
       <div class="container">
         <div class="container">
           <img v-if="bannerUrl" :src="bannerUrl" class="img-fluid banner" width="1200" height="265" alt="" />
-          <h1 class="page-title bg-black text-white">{{ metadataConfig.page_title }}</h1>
+          <h1 class="page-title text-white">{{ metadataConfig.page_title }}</h1>
         </div>
         <PaginatedTable
           :sorter="sorterMethod"
