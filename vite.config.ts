a5045640de0ddbeff4c906a50f381823719165b7
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-09-04 12:32:10 -0400

    Exclude main.js

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ command, mode }) => {
     test: {
       coverage: {
         include: ['app/**/*.js', 'app/**/*.ts', 'app/**/*.vue'],
-        exclude: ['app/configs/*.js']
+        exclude: ['app/configs/*.js', 'app/main.js']
       }
     },
     base: env.VITE_STATIC_TABLES_BASE_PATH
